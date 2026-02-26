86d5b3b67f758d96387a8908e26d4f0981ee4b63
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-02-26 12:37:55 -0600

    Update to v0.2.4 - fix admin command visibility

--- Formula/askkaya.rb
+++ Formula/askkaya.rb
@@ -1,45 +1,34 @@
 class Askkaya < Formula
   desc "AskKaya - Full-stack client support platform CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.2.3"
+  version "0.2.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.3/askkaya-darwin-arm64"
-      sha256 "d2403e6c706530450342334ccb363610ec42905a8d4aa10a4944597b4f29f234"
+      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.4/askkaya-darwin-arm64"
+      sha256 "7bc3c146b72ad703e8e782a6ac1414445abc928dfb22ec0cce43ed7614c5f007"
     else
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.3/askkaya-darwin-amd64"
-      sha256 "27960dbdcf53e4b1cb51c0ebfbbddc8ea5d314e243d133aa2231d75c8820893a"
+      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.4/askkaya-darwin-amd64"
+      sha256 "0884112c855b403918abe1f48cd91e3106371a69ebed3351b47e728d530e72e0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.3/askkaya-linux-arm64"
-      sha256 "e1f721fbe0f0652233ef268d957feb2241c9a6762623c9dcd5851a08b6681336"
+      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.4/askkaya-linux-arm64"
+      sha256 "40220e55bee6d95d1c6daa905af2b2692ee1684ba594ec33f8a341b89c5a0184"
     else
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.3/askkaya-linux-amd64"
-      sha256 "cc96d6bd553a8cf32509a5af47596794b324eff247ee0bebdd050fbdec8e3286"
+      url "https://github.com/kayacancode/askkaya/releases/download/v0.2.4/askkaya-linux-amd64"
+      sha256 "98686cb4fdc32a3be1b4dc833ae03e6b03dcbfd53f15c4b207a3c61be761ec56"
     end
   end
 
   def install
-    if OS.mac?
-      if Hardware::CPU.arm?
-        bin.install "askkaya-darwin-arm64" => "askkaya"
-      else
-        bin.install "askkaya-darwin-amd64" => "askkaya"
-      end
-    elsif OS.linux?
-      if Hardware::CPU.arm?
-        bin.install "askkaya-linux-arm64" => "askkaya"
-      else
-        bin.install "askkaya-linux-amd64" => "askkaya"
-      end
-    end
+    binary_name = "askkaya-#{OS.kernel_name.downcase}-#{Hardware::CPU.arch}"
+    bin.install Dir["askkaya-*"].first => "askkaya"
   end
 
   test do
-    system "#{bin}/askkaya", "--version"
+    assert_match "askkaya version", shell_output("#{bin}/askkaya --version")
   end
 end
