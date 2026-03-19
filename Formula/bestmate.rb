e5299a1490d776d312cd65875c0e4a2e9f202d03
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-03-19 14:48:01 -0500

    fix: update formula URLs to use public homebrew-askkaya releases

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -5,10 +5,10 @@ class Bestmate < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.4.0/bestmate-darwin-arm64"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.4.0/bestmate-darwin-arm64"
       sha256 "401efed2d56bfc0afa7d5296d36fe3e08c2ff66f155c4afa69933f3960ce4515"
     else
-      url "https://github.com/kayacancode/askkaya/releases/download/v0.4.0/bestmate-darwin-amd64"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.4.0/bestmate-darwin-amd64"
       sha256 "41f4b74544059c855f915ed514113bc34d6c1ebf93da1c31ed624979f895fab4"
     end
   end
