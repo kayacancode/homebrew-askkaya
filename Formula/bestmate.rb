ef901672477e967d5b4bd3d68cf04ae4651e62c4
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-03-31 15:20:04 -0500

    v0.6.2: fix Google OAuth binary

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,14 +1,14 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.6.1"
+  version "0.6.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.1/bestmate-darwin-arm64"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.2/bestmate-darwin-arm64"
       sha256 "5e53243ad8ddc515521f6fdf056ae14b2756098b9c54d7ae97c1fb7b1b1026e1"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.1/bestmate-darwin-amd64"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.2/bestmate-darwin-amd64"
       sha256 "364281b46eda14a4597c6bbf7e55f4e00c794992cbe7bf254f126337ddd33493"
     end
   end
