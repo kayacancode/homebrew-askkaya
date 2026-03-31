df6b937ae6969f532d4a46e9abbdaafb8e07149b
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-03-31 10:47:37 -0500

    v0.6.0: add deploy and export-chats commands

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,15 +1,15 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.5.0"
+  version "0.6.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.5.0/bestmate-darwin-arm64"
-      sha256 "6d8339c35ffd71efe3241be853d66cbbc87afe15bfbeb0c4dc194c6d717a2e44"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.0/bestmate-darwin-arm64"
+      sha256 "853eaaabee3543aa7bd5113c42ffc740856895d2c0d71cae68bb8279061b94eb"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.5.0/bestmate-darwin-amd64"
-      sha256 "83bc375124408f8bceeaa57e5a2ac78e48427660b41a79f61a9e903ce289a593"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.0/bestmate-darwin-amd64"
+      sha256 "21ad9f39d59d3ff67134bcf9fc79cf52b65e35a70ec53add9c0da2abd0c4c5fc"
     end
   end
 
