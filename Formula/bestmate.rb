2f537d3dd1620f922db26f4b3ba904c875fe5f2e
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-01 16:40:57 -0500

    v0.6.3: directory ingest, vault-private flag

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,15 +1,15 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.6.2"
+  version "0.6.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.2/bestmate-darwin-arm64"
-      sha256 "cfe29509408969f584a26644b3caebd742fcbd6a9ca9177795d1e5457b9995f3"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.3/bestmate-darwin-arm64"
+      sha256 "2f2b339e8840a8493b63f8f61b5c94952ef1408b5ca67e798af3497e06817412"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.2/bestmate-darwin-amd64"
-      sha256 "6a7d1bf943e9968bb8ce22687847cc0ab9a1b52e06181d75156a6d117cde2d66"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.6.3/bestmate-darwin-amd64"
+      sha256 "4cfc82ea6efea6e58a365f6eea251558a4f86af43e264d29d40912bc3489d34a"
     end
   end
 
