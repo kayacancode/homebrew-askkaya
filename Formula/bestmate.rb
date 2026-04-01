282bcd997bd30575a1663ce174185d5f7bd6bdff
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-01 17:03:16 -0500

    v0.7.0: fix amd64 hash

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -9,7 +9,7 @@ class Bestmate < Formula
       sha256 "c03a31482d94eddd090c244257cf9794af9b15486b46205d01f97dcad8737fce"
     else
       url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.7.0/bestmate-darwin-amd64"
-      sha256 "c03a31482d94eddd090c244257cf9794af9b15486b46205d01f97dcad8737fce"
+      sha256 "a8ae9a053bbca22f8ef4cb0f9513f4b0c5803a40d0958113df59e5a597df1ed1"
     end
   end
 
