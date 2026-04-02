5f84d8ee9698dfcadbb738abff63d303804f3bc2
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-02 11:22:16 -0500

    v0.9.1: rate limit + cache display

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,15 +1,15 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.9.0"
+  version "0.9.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.0/bestmate-darwin-arm64"
-      sha256 "9dff7fbf4f1e46700bea3331fe69d10794da9d6bb28266638eaab581579ddaa0"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.1/bestmate-darwin-arm64"
+      sha256 "a6c849912d72afb7edfcb935943aa6867242666c132eac79697ca08c1fcac668"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.0/bestmate-darwin-amd64"
-      sha256 "3879c27bda85362eb05b3caf682cfa8a2399b901876ebf097191b00dcdbdead8"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.1/bestmate-darwin-amd64"
+      sha256 "2dd58ab201d776b0a2959a62a42db87fafbeb691ef91816010216b020b22b7e5"
     end
   end
 
