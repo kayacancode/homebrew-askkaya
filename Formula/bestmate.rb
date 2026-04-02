f9e29e0a1b8167b969e794ac69e3223994cd955e
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-02 07:01:37 -0500

    v0.9.0: Twin Fidelity Evals

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,15 +1,15 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.8.0"
+  version "0.9.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.8.0/bestmate-darwin-arm64"
-      sha256 "c03a31482d94eddd090c244257cf9794af9b15486b46205d01f97dcad8737fce"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.0/bestmate-darwin-arm64"
+      sha256 "9dff7fbf4f1e46700bea3331fe69d10794da9d6bb28266638eaab581579ddaa0"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.8.0/bestmate-darwin-amd64"
-      sha256 "a8ae9a053bbca22f8ef4cb0f9513f4b0c5803a40d0958113df59e5a597df1ed1"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.9.0/bestmate-darwin-amd64"
+      sha256 "3879c27bda85362eb05b3caf682cfa8a2399b901876ebf097191b00dcdbdead8"
     end
   end
 
