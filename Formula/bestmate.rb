8a32a2ff528cf8ac3daab1d74908cc0be77fe28e
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-14 12:37:33 -0500

    bump to v0.12.1: fix auth file-based token storage

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -1,15 +1,15 @@
 class Bestmate < Formula
   desc "Bestmate - Your AI knowledge twin CLI"
   homepage "https://github.com/kayacancode/askkaya"
-  version "0.12.0"
+  version "0.12.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.0/bestmate-darwin-arm64"
-      sha256 "1512245dfc9c56f7e2612b9074353125623c8c63a1d280f824ee70f4cc99c637"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.1/bestmate-darwin-arm64"
+      sha256 "505cb2374ee3a294cc612d773301cb1aba3a5707dd59b90124c79f8ca688cddc"
     else
-      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.0/bestmate-darwin-arm64"
-      sha256 "1512245dfc9c56f7e2612b9074353125623c8c63a1d280f824ee70f4cc99c637"
+      url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.1/bestmate-darwin-amd64"
+      sha256 "166d6f1289471bc4c806824822122e5aaf33de8aa9131fe3303c9bfa6e204920"
     end
   end
 
@@ -18,7 +18,7 @@ class Bestmate < Formula
       if Hardware::CPU.arm?
         bin.install "bestmate-darwin-arm64" => "bestmate"
       else
-        bin.install "bestmate-darwin-arm64" => "bestmate"
+        bin.install "bestmate-darwin-amd64" => "bestmate"
       end
     end
   end
