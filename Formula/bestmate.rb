203267ed79fe0fdb756d51666b1213e70c3dc97f
kayacancode <62274079+kayacancode@users.noreply.github.com>
2026-04-17 15:31:33 -0500

    update SHA256 for v0.12.1

--- Formula/bestmate.rb
+++ Formula/bestmate.rb
@@ -6,10 +6,10 @@ class Bestmate < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.1/bestmate-darwin-arm64"
-      sha256 "3eef2f20ca1ee434b1ad8c0d1db15b065a9636635c6e60e6a012c4ef805b3110"
+      sha256 "3286d40ca6f63180e24a80e89e7e2ce461b8fd84d14414fe8e476794e6e6e533"
     else
       url "https://github.com/kayacancode/homebrew-askkaya/releases/download/v0.12.1/bestmate-darwin-amd64"
-      sha256 "58e248475a9a258591d685e0946bdeb012d7cb6ee8c8a4271b990c92cc734e71"
+      sha256 "1e9279a24fe8393030bd0b9cc93dd96362e48f125c634e210308d3f28c872ec7"
     end
   end
 
